f2b45df89d0197a65aff8f61bc5b4d92d367ba73
Patrick Yoon <yeyoon@cis.upenn.edu>
2013-04-15 05:09:09 -0400

    schedulers

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from private_config import *
 from gmail_imap import *
 from gmail_smtp import gmail_smtp
+from apscheduler.scheduler import Scheduler
+
+sched = Scheduler()
+email_sent = False
+          
+@sched.interval_schedule(seconds=30)
+def check_email():
+    global email_sent
+    if not email_sent:
+        gmail_imap = gmail_imap(GMAIL_USERNAME, GMAIL_PASSWORD)
+        gmail_smtp = gmail_smtp(GMAIL_USERNAME, GMAIL_PASSWORD, debug=True)
+        gmail_smtp.login()
+        gmail_imap.mailboxes.load()
+        gmail_imap.messages.process("INBOX")
+        print "\n"
+        #Print the full message text for the first two messages in the box
+        for message_stub in gmail_imap.messages:
+          message = gmail_imap.messages.getMessage(message_stub.uid)
+          print message.From
+          print message.Body
+          if TARGET_FROM in message.From and re.search(r'formal', message.Body, re.I):
+              print "found"
+              gmail_smtp.send_email(TARGET_FROM, "Re: %s" %message.Subject, TARGET_BODY)
+              print "sent"
+        gmail_imap.logout()
+        gmail_smtp.logout()
 
 if __name__ == '__main__':
-    import getpass
- 
-    gmail_imap = gmail_imap(GMAIL_USERNAME, GMAIL_PASSWORD)
-    gmail_smtp = gmail_smtp(GMAIL_USERNAME, GMAIL_PASSWORD, debug=True)
-    gmail_smtp.login()
-    gmail_imap.mailboxes.load()
-    print gmail_imap.mailboxes
- 
-    gmail_imap.messages.process("INBOX")
-    print "\n"
-    #Print the full message text for the first two messages in the box
-    for message_stub in gmail_imap.messages:
-      message = gmail_imap.messages.getMessage(message_stub.uid)
-      print message.From
-      print message.Body
-      if TARGET_FROM in message.From and re.search(r'formal', message.Body, re.I):
-          print "found"
-          gmail_smtp.send_email(TARGET_FROM, "Re: %s" %message.Subject, TARGET_BODY)
-          print "sent"
-          email_sent = True
-    gmail_imap.logout()
-    gmail_smtp.logout()
+    sched.start()
+    while True:
+        pass
