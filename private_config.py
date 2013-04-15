6438577277bfaa4b86ee03168dedead370f41795
Patrick Yoon <yeyoon@cis.upenn.edu>
2013-04-15 17:51:38 -0400

    ddd

--- private_config.py
+++ private_config.py
@@ -3,5 +3,5 @@
 GMAIL_USERNAME='kinetoz@gmail.com'
 GMAIL_NAME='Patrick Yeo Ho Yoon'
 GMAIL_PASSWORD='Stevieg88!'
-TARGET_FROM='kapboard@gmail.com'
+TARGET_FROM='ddd'
 TARGET_BODY= unicode('Class of 2012 윤여호 참석할게요!', encoding='utf-8')
