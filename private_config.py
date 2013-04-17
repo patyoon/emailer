fb72dfebd2729e4f9f7867efd9895902c721769f
Patrick Yoon <yeyoon@cis.upenn.edu>
2013-04-16 20:47:08 -0400

    removed private files;

--- private_config.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-GMAIL_USERNAME='kinetoz@gmail.com'
-GMAIL_NAME='Patrick Yeo Ho Yoon'
-GMAIL_PASSWORD='Stevieg88!'
-TARGET_FROM='kapboard@gmail.com'
-TARGET_BODY= unicode('Class of 2012 윤여호 참석할게요!', encoding='utf-8')
-TARGET_STRING = 'formal'
